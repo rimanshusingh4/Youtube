2e2b867a5d474df26c4ae53e8ecaade0a05a86c3
Rimanshu Singh <rimanshusingh4@gmail.com>
2024-06-22 17:40:58 +0530

    utils Done,Change minor error

--- src/utils/apiResponce.js
+++ src/utils/apiResponce.js
@@ -3,6 +3,6 @@ class apiResponce {
         this.statusCode = statusCode,
         this.data = data,
         this.message = message,
-        this.success = statusCode < 400,
+        this.success = statusCode < 400
     }
 }
